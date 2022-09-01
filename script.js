8240b500f6d4b31a99daf7b436089950ee4dd856
manikandan9500 <111909776+manikandan9500@users.noreply.github.com>
2022-09-01 15:13:47 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+// / var Container=document.createElement("div");
+// Container.setAttribute("class","container");
+
+// var row=document.createElement("div");
+// row.setAttribute("class","row");
+
+// var col=document.createElement("div");
+// col.setAttribute("class","col");
+
+// row.append(col);
+// Container.append(row);
+// document.body.append(Container);
+
+function foo() {
+    var  res=document.getElementById("fristname").Value;
+    console.log(res$(firstname.madilename.lastname.email));
+}
+foo()
